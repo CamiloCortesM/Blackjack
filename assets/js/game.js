80c5b44470e91c0bda83aad7a8e2f960802a7e0d
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-27 12:06:01 -0500

    return card value

--- assets/js/game.js
+++ assets/js/game.js
@@ -38,3 +38,8 @@ const orderCard = () => {
 };
 
 console.log(orderCard());
+
+const cardValue = (card) => {
+  const value = card.substring(0, card.length - 1);
+  return isNaN(value) ? (value === "A" ? 11 : 10) : value * 1;
+};
