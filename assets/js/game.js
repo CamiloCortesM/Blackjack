eeff94ff86efbdc7df55993876e145de296885e8
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-03-28 22:48:44 -0500

    application-wide optimizations

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,23 +1,37 @@
-(() => {
+const Module = (() => {
   "use strict";
 
   let deck = [];
-  const types = ["C", "D", "H", "S"];
-  const specials = ["A", "J", "Q", "K"];
+  const types = ["C", "D", "H", "S"],
+    specials = ["A", "J", "Q", "K"];
 
-  let pointsPlayer = 0,
-    pointsComputer = 0;
+  let pointsPlayers = [];
 
   //references of HTML
 
-  const btnOrder = document.querySelector("#btnOrder");
-  const btnStop = document.querySelector("#btnStop");
-  const btnNew = document.querySelector("#btnNew");
-  const score = document.querySelectorAll("small");
-  const divCardsPlayer = document.querySelector("#player-cards");
-  const divCardsComputer = document.querySelector("#computer-cards");
+  const btnOrder = document.querySelector("#btnOrder"),
+    btnStop = document.querySelector("#btnStop"),
+    btnNew = document.querySelector("#btnNew");
+
+  const divCardPlayers = document.querySelectorAll(".divCards"),
+    score = document.querySelectorAll("small");
+
+  const initGame = (numPlayers = 2) => {
+    deck = createDeck();
+
+    pointsPlayers = [];
+    for (let i = 0; i < numPlayers; i++) {
+      pointsPlayers.push(0);
+    }
+
+    score.forEach((elem) => (elem.innerText = 0));
+    divCardPlayers.forEach((elem) => (elem.innerHTML = ""));
+    btnStop.disabled = false;
+    btnOrder.disabled = false;
+  };
 
   const createDeck = () => {
+    deck = [];
     for (let i = 2; i <= 10; i++) {
       // deck.push(i + "C");
       for (let type of types) {
@@ -30,12 +44,8 @@
         deck.push(sp + type);
       }
     }
-    deck = _.shuffle(deck);
-
-    return deck;
+    return _.shuffle(deck);
   };
-
-  createDeck();
 
   const orderCard = () => {
     if (deck.length === 0) {
@@ -49,25 +59,29 @@
     return isNaN(value) ? (value === "A" ? 11 : 10) : value * 1;
   };
 
-  //turn of machine
-  const turnComputer = (minimumPoints) => {
-    do {
-      const card = orderCard();
-      pointsComputer = pointsComputer + cardValue(card);
-      score[1].innerText = pointsComputer;
-      const imgCard = document.createElement("img");
-      imgCard.src = `assets/cards/${card}.png`;
-      imgCard.alt = "card";
-      imgCard.classList.add("item_card");
-      divCardsComputer.append(imgCard);
-    } while (pointsComputer < minimumPoints && minimumPoints < 21);
+  //turn: 0 = firts player and last number is the computer
+  const accumulatePoints = (card, turn) => {
+    pointsPlayers[turn] = pointsPlayers[turn] + cardValue(card);
+    score[turn].innerText = pointsPlayers[turn];
+    return pointsPlayers[turn];
+  };
 
+  const createCard = (card, turn) => {
+    const imgCard = document.createElement("img");
+    imgCard.src = `assets/cards/${card}.png`;
+    imgCard.alt = "card";
+    imgCard.classList.add("item_card");
+    divCardPlayers[turn].append(imgCard);
+  };
+
+  const determineWinner = () => {
+    const [minimumPoints, pointsComputer] = pointsPlayers;
     setTimeout(() => {
       alert(
-        (pointsComputer > pointsPlayer && pointsComputer < 21) ||
-          pointsPlayer > 21
+        (pointsComputer > minimumPoints && pointsComputer < 21) ||
+          minimumPoints > 21
           ? "You Lose"
-          : (pointsPlayer > pointsComputer && pointsPlayer <= 21) ||
+          : (minimumPoints > pointsComputer && minimumPoints <= 21) ||
             pointsComputer > 21
           ? "You win"
           : "Nobody won"
@@ -75,19 +89,25 @@
     }, 100);
   };
 
+  //turn of machine
+  const turnComputer = (minimumPoints) => {
+    do {
+      const card = orderCard();
+      accumulatePoints(card, pointsPlayers.length - 1);
+      createCard(card, pointsPlayers.length - 1);
+    } while (
+      pointsPlayers[pointsPlayers.length - 1] < minimumPoints &&
+      minimumPoints <= 21
+    );
+    determineWinner();
+  };
+
   //Events
 
   btnOrder.addEventListener("click", () => {
     const card = orderCard();
-    pointsPlayer = pointsPlayer + cardValue(card);
-    score[0].innerText = pointsPlayer;
-    /* <img class="item_card" src="assets/cards/10D.png" alt="card" /> */
-
-    const imgCard = document.createElement("img");
-    imgCard.src = `assets/cards/${card}.png`;
-    imgCard.alt = "card";
-    imgCard.classList.add("item_card");
-    divCardsPlayer.append(imgCard);
+    const pointsPlayer = accumulatePoints(card, 0);
+    createCard(card, 0);
 
     if (pointsPlayer > 21) {
       btnStop.disabled = true;
@@ -104,19 +124,12 @@
   btnStop.addEventListener("click", () => {
     btnStop.disabled = true;
     btnOrder.disabled = true;
-    turnComputer(pointsPlayer);
+    turnComputer(pointsPlayers[0]);
   });
 
   btnNew.addEventListener("click", () => {
-    pointsPlayer = 0;
-    pointsComputer = 0;
-    score[0].innerText = "0";
-    score[1].innerText = "0";
-    deck = [];
-    createDeck();
-    divCardsPlayer.innerHTML = "";
-    divCardsComputer.innerHTML = "";
-    btnStop.disabled = false;
-    btnOrder.disabled = false;
+    initGame();
   });
+
+  return { initGame };
 })();
