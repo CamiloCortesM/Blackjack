769b0640909e19bd122ece035c73778924060297
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-04-04 22:41:04 -0500

    JSdoc comments with create deck module

--- src/blackjack/usecases/create-deck.js
+++ src/blackjack/usecases/create-deck.js
@@ -1,6 +1,14 @@
 import { shuffle } from "underscore";
 
+/**
+ *this function create a new deck
+ * @param {Array<String>} types Example: ["C", "D", "H", "S"]
+ * @param {Array<String>} specials Example: ["A", "J", "Q", "K"]
+ * @returns {Array<String>} return a new cards deck
+ */
 export const createDeck = (types, specials) => {
+  if (!types || types.length===0) throw new Error("Types is required");
+  if (!specials) throw new Error("specials is required");
   let deck = [];
   for (let i = 2; i <= 10; i++) {
     // deck.push(i + "C");
