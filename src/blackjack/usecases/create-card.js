a1b7ec639847317fb30901744f31c6174ef3d775
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-04-05 23:44:14 -0500

    create card module

---
+++ src/blackjack/usecases/create-card.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * @param {String} card
+ * @param {Number} turn
+ * @param {Object} divCardPlayers
+ */
+
+export const createCard = (card, turn, divCardPlayers) => {
+  const imgCard = document.createElement("img");
+  imgCard.src = `assets/cards/${card}.png`;
+  imgCard.alt = "card";
+  imgCard.classList.add("item_card");
+  divCardPlayers[turn].append(imgCard);
+};
