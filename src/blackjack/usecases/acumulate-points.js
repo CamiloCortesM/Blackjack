d0b5f5d7fa6d304fca5d628938265ed195dfafd1
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-04-05 23:44:37 -0500

    acumulate points module

---
+++ src/blackjack/usecases/acumulate-points.js
@@ -0,0 +1,15 @@
+import { cardValue } from "./card-value";
+
+/**
+ *
+ * @param {String} card
+ * @param {Number} turn
+ * @param {Array<Number>} pointsPlayers
+ * @param {Object} score
+ * @returns {Number}
+ */
+export const accumulatePoints = (card, turn, pointsPlayers, score) => {
+  pointsPlayers[turn] = pointsPlayers[turn] + cardValue(card);
+  score[turn].innerText = pointsPlayers[turn];
+  return pointsPlayers[turn];
+};
