6d282f9e2ee98fe34a083573bc05f63038d7f355
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-04-05 23:44:01 -0500

    barrel file

--- src/blackjack/usecases/index.js
+++ src/blackjack/usecases/index.js
@@ -1,3 +1,6 @@
 export * from "./card-value.js";
 export * from "./create-deck.js";
 export * from "./order-card.js";
+export * from "./acumulate-points.js";
+export * from "./turn-computer.js";
+export * from "./create-card.js";
