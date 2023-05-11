5c8b0b7c573a3b9bda1290f3fcc296b97744b0f6
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-05-11 14:12:25 -0500

    Add image compression option to Vite configuration file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,28 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  base: "/",
-});
+// vite.config.js
+export default {
+  build: {
+    minify: true,
+    outDir: 'dist',
+    assetsInlineLimit: 4096,
+    assetsDir: 'assets',
+    cssCodeSplit: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+    // Add this option to compress images
+    compress: {
+      // Specify the level of compression you want to apply
+      // 0 is the lowest level of compression and 9 is the highest level of compression
+      level: 9,
+      // Specify the file types you want to compress
+      filetypes: ['jpg', 'jpeg', 'png', 'svg'],
+    },
+  },
+};
